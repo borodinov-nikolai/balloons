3670bdf464c5340ce573646ccd32661c36b19ad2
Valera Andriyanov <mprint85@gmail.com>
2022-08-27 17:02:35 +0300

    work

--- frontend/lib/media.ts
+++ frontend/lib/media.ts
@@ -2,8 +2,9 @@ import { ImageType } from "types/general"
 
 export function getMediaUrl(img?: ImageType): string {
   const url = img?.url
+
   if (url) {
-    const baseURL = process.env.PUBLIC_URL
+    const baseURL = process.env.NEXT_PUBLIC_URL
     return `${baseURL || "http://localhost:1337"}${url}`
   }
   return ""
