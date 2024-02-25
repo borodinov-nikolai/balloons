ad66d4558491b781bfabce00c5c9db61c7e6ed50
Lorneyq <nikita.needle886@gmail.com>
2024-02-25 16:28:55 +0300

    backend edits

--- backend/src/api/release/controllers/release.ts
+++ backend/src/api/release/controllers/release.ts
@@ -1,7 +1,43 @@
 /**
- *  release controller
+ * release controller
  */
 
 import { factories } from "@strapi/strapi"
 
-export default factories.createCoreController("api::release.release")
+const { createCoreController } = require("@strapi/strapi").factories
+
+module.exports = createCoreController("api::release.release", ({ strapi }) => ({
+  async findMy(ctx) {
+    const entity = await strapi.db.query("api::release.release").findOne({
+      where: { user: { id: ctx.state.user.id } },
+      populate: "*",
+    })
+
+    ctx.body = entity
+  },
+
+  async delete(ctx) {
+    const { id } = ctx.params
+
+    const release = await strapi.query("api::release.release").findOne({
+      id,
+      populate: {
+        user: true,
+      },
+    })
+
+    if (release.id != id) {
+      return ctx.notFound("Release not found")
+    }
+
+    if (release.user.id !== ctx.state.user.id) {
+      return ctx.forbidden("You do not have permission to delete this release")
+    }
+
+    const deletedEntity = await strapi.query("api::release.release").delete({
+      where: { id: id },
+    })
+
+    return ctx.send(deletedEntity)
+  },
+}))
